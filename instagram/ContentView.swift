7c33ae49ed543a34cf5140853f6ca05bbcb9e8c1
Edward Chung <edward.chung@redfin.com>
2019-11-22 10:59:48 -0800

    building the ui of one post complete

--- instagram/ContentView.swift
+++ instagram/ContentView.swift
@@ -10,7 +10,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            HStack() {
+                Image("banff")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .clipShape(Circle())
+                VStack(alignment: .leading) {
+                    Text("zerotoappstore")
+                        .font(.headline)
+                    Text("San Francisco").font(.subheadline)
+                }
+            }.padding(.horizontal, 10)
+            
+            Image("sfbeach")
+                .resizable()
+                .frame(height: 630)
+                .aspectRatio(contentMode: .fit)
+            VStack(alignment: .leading, spacing: 8) {
+                HStack(spacing: 20) {
+                    Image(systemName: "suit.heart")
+                        .resizable()
+                    .frame(width: 30, height: 30)
+                    Image(systemName: "message")
+                        .resizable()
+                    .frame(width: 30, height: 30)
+                    Image(systemName: "paperplane")
+                        .resizable()
+                    .frame(width: 30, height: 30)
+                    Spacer()
+                    Image(systemName: "bookmark")
+                    .resizable()
+                    .frame(width: 25, height: 30)
+                }
+                
+                Text("100 likes").font(.headline)
+                HStack {
+                    Text("zerotoappstore")
+                        .font(.headline)
+                    Text("I like long walks on the beach")
+                }
+                Text("View all 400 comments")
+                    .foregroundColor(Color.gray)
+            }.padding(.horizontal, 15)
+            Spacer()
+        }
     }
 }
 
